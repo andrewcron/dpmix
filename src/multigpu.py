d89ae1d90c30a484c3342b020adfb53707fbef64
Andrew Cron <andrew.j.cron@gmail.com>
2012-04-13 15:36:33 -0400

    Revert "multiprocess doesn twork ... will revert"
    This reverts commit fc02a9d3886f8c72262aabd82348fbe51f65fd12.

--- src/multigpu.py
+++ src/multigpu.py
@@ -3,61 +3,66 @@ Support for multi-GPU via threading for dpmix MCMC
 Written by: Andrew Cron
 """
 
-#import threading
-import multiprocessing
+import threading
 import numpy as np
+import pycuda.autoinit
+import pycuda.driver as drv
+from pycuda.gpuarray import to_gpu
+import gpustats
+import gpustats.sampler
+from cuda_functions import *
 
-#import pycuda.autoinit
-#import gpustats
-#import gpustats.sampler
-#from cuda_functions import *
+class GPUWorker(threading.Thread):
 
-class GPUWorker(multiprocessing.Process):
-
-    def __init__(self, data, device):
-        multiprocessing.Process.__init__(self)
+    def __init__(self, data, w, mu, Sigma, relabel, device):
+        threading.Thread.__init__(self)
 
         self.data = data
-        self.nobs, self.ndim = self.data.shape
+        self.w = w.flatten()
+        self.mu = mu
+        self.Sigma = Sigma
         self.device = device
-        self.tasks = multiprocessing.Queue()
-        self.results = multiprocessing.Queue()
+        self.relabel = relabel
+        self.nobs, self.ndim = data.shape
+        self.ncomp = len(w)
+        self.condition = threading.Condition()
+        self.end_sampler = False
+        self.new_params = False
 
     def run(self):
 
-        import pycuda.autoinit
-        from pycuda.gpuarray import to_gpu
-        #import pycuda.driver as drv
-        #drv.init()
-        #self.dev = drv.Device(self.device)
-        #self.ctx = self.dev.make_context()
-
-
-
+        self.condition.acquire()
+        self.dev = drv.Device(self.device)
+        self.ctx = self.dev.make_context()
         ## load my portion of data to gpu
         self.gdata = to_gpu(np.asarray(self.data, dtype=np.float32))
+        self.condition.release()
         densities = 0
 
-        ## this can only be killed by externally by ending None in the queue
-        while True:
-            ## wait for new params ...
-            new_task = self.tasks.get()
-            if new_task is None:
-                break # poison pill
-
+        ## this can only be killed by externally setting end_sampler to True
+        ## and releasing the lock
+        while not self.end_sampler:
+            ## wait for new params ... the model params should have changed
+            self.condition.acquire()
+            while not self.new_params and not self.end_sampler:
+                self.condition.wait()
+            if self.end_sampler:
+                break
             ## get new samples and maybe row max
-            densities = gpustats.mvnpdf_multi(self.gdata, new_task.mu, new_task.Sigma,
-                                              weights = new_task.w.flatten(), get=False, 
-                                              logged=True, order='C')
+            densities = gpustats.mvnpdf_multi(self.gdata, self.mu, self.Sigma,
+                                              weights = self.w, get=False, logged=True,
+                                              order='C')
             self.labs = gpustats.sampler.sample_discrete(densities, logged=True)
-            #print self.labs
-            #print densities
-            if new_task.relabel:
+            print self.labs
+            print densities
+            if self.relabel:
                 Z = gpu_apply_row_max(densities)[1].get()
             else:
-                Z = None                
+                Z = None
+                
+            self.new_params = False
+            self.condition.release()
             ## host thread should gather new labels and proceed. 
-            self.results.put(self.labs.copy())
 
         del self.gdata
         del densities
@@ -65,7 +70,6 @@ class GPUWorker(multiprocessing.Process):
         del self.ctx
 
 def init_GPUWorkers(data, w, mu, Sigma, devslist=None):
-    # partitions data
     nobs, ndim = data.shape
     lenpart = nobs / len(devslist)
     partitions = range(0, nobs, lenpart); 
@@ -74,44 +78,47 @@ def init_GPUWorkers(data, w, mu, Sigma, devslist=None):
     else:
         partitions[-1] = nobs
     
-    #launch threads uploading GPU data
+    #launch threads
     i=0; workers = []
     for dev in devslist:
         workers.append(GPUWorker(data[partitions[i]:partitions[i+1]], 
+                                 w=w, mu=mu, Sigma=Sigma, relabel=False,
                                  device=dev))
         workers[-1].start()
         i+=1
     return workers
 
-# simple object to go into task queue
-class Task(object):
-    def __init__(self, w, mu, Sigma):
-        self.w = w
-        self.mu = mu
-        self.Sigma = Sigma
-
-# callable function of new params and init threads
 def get_labels(workers, w, mu, Sigma):
     # run all the threads
     nobs, i = 0, 0
     partitions = [0]
     for thd in workers:
+        # get control of thread
+        thd.condition.acquire()
         # give new params
         nobs += thd.nobs
         partitions.append(nobs)
-        thd.tasks.put(Task(w, mu, Sigma))
+        thd.w = w
+        thd.mu = mu
+        thd.Sigma = Sigma
+        thd.new_params = True
+        thd.condition.notify()
+        thd.condition.release()
     #gather the results
     res = np.zeros(nobs, dtype=np.float32)
     for thd in workers:
-        labs = thd.results.get()
-        res[partitions[i]:partitions[i+1]] = labs
+        thd.condition.acquire() # wait until finished
+        res[partitions[i]:partitions[i+1]] = thd.labs.copy()
+        thd.condition.release() 
         i+=1
     return res
 
-# feeds poison pills to each thread
 def kill_workers(workers):
     for thd in workers:
-        thd.tasks.put(None)
+        thd.condition.acquire()
+        thd.end_sampler = True
+        thd.condition.notify()
+        thd.condition.release()
     
         
 
