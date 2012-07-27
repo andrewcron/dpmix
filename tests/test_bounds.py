4525c18cc09b25189f54ee0159e153aa5beeb51d
Andrew Cron <andrew.j.cron@gmail.com>
2012-07-26 20:12:24 -0400

    test cases to work with on some bounds and memory issues

---
+++ tests/test_bounds.py
@@ -0,0 +1,24 @@
+import numpy.random as npr
+from dpmix import HDPNormalMixture
+
+if __name__ == '__main__':
+    nclust = 256
+    niter = 10
+    burnin = 10
+    device = 2
+    max_events = 50000
+    num_files = 10
+
+    seed = 9
+    npr.seed(seed)
+
+    xs = []
+    for i in range(num_files):
+        print i,
+        xs.append(npr.uniform(-5,5,(max_events, 5)))
+    print
+
+    mcmc = HDPNormalMixture(xs, ncomp=nclust, gpu=device, parallel=True, verbose=2)
+    mcmc.sample(burnin, nburn=0, tune_interval=5)
+    imcmc = HDPNormalMixture(mcmc, verbose=2)
+    imcmc.sample(niter, nburn=0, ident=True)
