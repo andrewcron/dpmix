362c25c7f2bd6736b65b8e1ad69746ea6d179eee
Andrew Cron <andrew.j.cron@gmail.com>
2012-10-31 16:41:32 -0400

    readme updated

--- README.rst
+++ README.rst
@@ -33,3 +33,38 @@ Usage
 
 Check out the class docstrings for more info.
 
+MPI
+---
+
+The multigpu facilities are developed using MPI. Therefore, 
+using multiple machines is possible. However, note that the
+machines must be configured the same way. (Python)
+
+Running the code on multiple machines requires *mpiexec*:
+
+::
+
+   mpiexec -hostfile my_hosts -np 1 python tests/test_dpmix.py --gpu MPI
+
+Where the *my_hosts* file looks like 
+
+::
+
+   host1 slots=3
+   host2 slots=2
+
+I'm assuming here that the master instance of python is running on host1
+and that host1 and host2 have 2 GPUs each. Note, an extra slot needs to be
+reserved for the master on host1. Furthermore, we need to specify which
+devices to use on each host. The *gpu* argument in the class constructors
+must be a dictionary like
+
+::
+
+  gpu={'host1': [0,1], 'host2': [0,1]}
+
+The keys must match the result of a call to *os.uname()* to get the
+host string.
+
+
+
--- tests/test_hdp.py
+++ tests/test_hdp.py
@@ -20,7 +20,7 @@ from hdp import HDPNormalMixture
 if __name__ == '__main__':
 
     J = 20
-    N = int(1e6)
+    N = int(1e7)
     K = 2
     ncomps = 3
     true_labels, data = generate_data(n=N, k=K, ncomps=ncomps)
@@ -32,15 +32,14 @@ if __name__ == '__main__':
     data = [ all_data[(N/J*i):(N/J*(i+1))].copy() for i in range(J) ]
 
     #mcmc = HDPNormalMixture(data, ncomp=3, gpu=[0,1,2], parallel=True, verbose=100)
-    mcmc = HDPNormalMixture(data, ncomp=3, parallel=True, verbose=100,
-                            gpu={'lilo':[0,1,2,3,4], 'stitch':[0,1,2,3,4]})
-    mcmc.sample(100, nburn=1000, tune_interval=50)
+    mcmc = HDPNormalMixture(data, ncomp=100, parallel=True, verbose=1,gpu=[0,1,2,3,4])
+    mcmc.sample(2, nburn=1, tune_interval=50)
     #import pdb; pdb.set_trace()
-    imcmc = HDPNormalMixture(mcmc, verbose=100)
-    imcmc.sample(100, nburn=0, ident=True)
-    print imcmc.mu[-1]
-    print imcmc.weights[-1]
-    print imcmc.beta[-1]
+    #imcmc = HDPNormalMixture(mcmc, verbose=100)
+    #imcmc.sample(100, nburn=0, ident=True)
+    #print imcmc.mu[-1]
+    #print imcmc.weights[-1]
+    #print imcmc.beta[-1]
 
 
 
